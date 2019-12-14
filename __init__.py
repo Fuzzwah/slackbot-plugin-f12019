494e78cf9e2db0b2a6ec51041a8fb05d1b2c7ee2
Fuzzwah <fuzzwah@github.com>
2019-12-14 13:26:33 +1100

    2020

--- __init__.py
+++ __init__.py
@@ -27,16 +27,16 @@ admin_commands_info["!all"] = "display all the races in the database"
 
 def command_next(user, chat_string, channel, teamid):
     # respond with a random item from our greeting messages from the database
-    event = Season2019.select().where(Season2019.race >= date.today()).order_by(Season2019.race).limit(1).get()
+    event = Events.select().where(Events.race >= date.today()).order_by(Events.race).limit(1).get()
     outputs.append([channel, "{}, {} - Qualifying @ {}, Race @ {}".format(event.name, event.city, dateutil.parser.parse(event.qual).strftime("%d %B, %H:%M"), dateutil.parser.parse(event.race).strftime("%d %B, %H:%M"))])
 
 def command_all(user, chat_string, channel, teamid):
-    for event in Season2019.select():
+    for event in Events.select():
         outputs.append([channel, "{}, {} - Qualifying @ {}, Race @ {}".format(event.name, event.city, dateutil.parser.parse(event.qual).strftime("%d %B, %H:%M"), dateutil.parser.parse(event.race).strftime("%d %B, %H:%M"))])
 
 def set_topic():
     r = {}
-    event = Season2019.select().where(Season2019.race >= date.today()).order_by(Season2019.race).limit(1).get()
+    event = Events.select().where(Events.race >= date.today()).order_by(Events.race).limit(1).get()
     r['cmd'] = "setTopic"
     r['channel'] = config['channel']
     r['string'] = "{}, {} - Qualifying @ {}, Race @ {}".format(event.name, event.city, dateutil.parser.parse(event.qual).strftime("%d %B, %H:%M"), dateutil.parser.parse(event.race).strftime("%d %B, %H:%M"))
