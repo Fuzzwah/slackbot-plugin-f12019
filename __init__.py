255c9260b4a25111863a7b3acffe3d30c02aa343
Fuzzwah <fuzzwah@github.com>
2019-12-14 13:41:57 +1100

    2020

--- __init__.py
+++ __init__.py
@@ -36,8 +36,8 @@ def command_all(user, chat_string, channel, teamid):
 
 def set_topic():
     r = {}
-    event = Events.select().where(Events.race >= date.today()).order_by(Events.race).limit(1).get()
-    print(event.sql())
+    print(Events.select().where(Events.race >= date.today()).order_by(Events.race).limit(1).sql())
+    exit()
     r['cmd'] = "setTopic"
     r['channel'] = config['channel']
     r['string'] = "{}, {} - Qualifying @ {}, Race @ {}".format(event.name, event.city, dateutil.parser.parse(event.qual).strftime("%d %B, %H:%M"), dateutil.parser.parse(event.race).strftime("%d %B, %H:%M"))
